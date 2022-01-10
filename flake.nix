952923d0f8178c7a75932259d6ece9822946de03
Preston Marshall <git@bbhoss.io>
2022-01-10 04:05:51 -0500

    nix docker image build

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Development environment";
+  description = "Hosscoinbot. The ultimate centralized shitcoin.";
 
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
@@ -15,11 +15,56 @@
       let
         inherit (nixpkgs.lib) optional;
         pkgs = import nixpkgs { inherit system; };
+        erlPkgs = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
+        src = builtins.fetchGit {
+          url = "ssh://git@github.com/bbhoss/hosscoinbot";
+          rev = "0ac87ae9c9c5fe676b92d77164f1167b89047f48";
+        };
+        pname = "hosscoinbot";
+        version = "0.0.1";
 
-        elixir = pkgs.beam.packages.erlang.elixir;
-        locales = pkgs.glibcLocales;
+        elixir = erlPkgs.elixir;
+        locales = pkgs.glibcLocales.override {
+          allLocales = false; # Only en-US utf8
+        };
+        mixFodDeps = erlPkgs.fetchMixDeps {
+          pname = "mix-deps-${pname}";
+          inherit src version;
+          sha256 = "2hrfG49grSJJStvSIRS/ZvIRyz2GfokObqsEz8Arg7Q=";
+        };
+        hosscoinbotRelease = erlPkgs.mixRelease {
+          inherit src pname version mixFodDeps;
+
+          depsBuildTarget = [
+            locales
+          ];
+        };
       in
       {
+        packages.hosscoinbot = hosscoinbotRelease;
+        packages.hosscoinbotImage = pkgs.dockerTools.buildImage {
+          name = pname;
+          tag = version;
+
+          contents = [
+            hosscoinbotRelease
+            pkgs.busybox # for debugging
+          ];
+
+          config = {
+            Cmd = [
+              "${hosscoinbotRelease}/bin/hosscoinbot"
+              "start"
+            ];
+            Env = [
+              "LC_ALL=en_US.UTF-8"
+              "LOCALE_ARCHIVE=${locales}/lib/locale/locale-archive"
+            ];
+          };
+        };
+
+        defaultPackage = self.packages.${system}.hosscoinbot;
+
         devShell = pkgs.mkShell {
           buildInputs = [
             elixir
