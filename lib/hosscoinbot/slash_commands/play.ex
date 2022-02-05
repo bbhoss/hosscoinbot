7f80b7acac53a3b3086f85e22a2889e3a580a63c
Preston Marshall <git@bbhoss.io>
2022-02-04 23:11:26 -0500

    defer interaction response to prevent timeout

--- lib/hosscoinbot/slash_commands/play.ex
+++ lib/hosscoinbot/slash_commands/play.ex
@@ -24,6 +24,7 @@ defmodule Hosscoinbot.SlashCommands.Play do
     options: [%{name: "url", type: 3, value: track_url}],
   }, guild_id: guild_id }) do
     {:ok, _pid} = Jukebox.ensure_started(guild_id)
+    Api.create_interaction_response(interaction, loading_response())
     interaction_user_id = interaction.member.user.id
     if !Voice.playing?(guild_id) do
       :ok = Jukebox.ensure_bot_in_proper_voice_channel(guild_id, interaction_user_id)
@@ -34,36 +35,33 @@ defmodule Hosscoinbot.SlashCommands.Play do
       {:error, msg} -> error_response(msg, track_url)
     end
 
-    Api.create_interaction_response(interaction, response)
+    Api.edit_interaction_response(interaction, response)
   end
 
   defp ok_response(track_url, :playing) do
     %{
-      type: 4,
       flags: 64, # Ephemeral
-      data: %{
-        content: "Playing track: #{track_url}"
-      }
+      content: "Playing track: #{track_url}"
     }
   end
 
   defp ok_response(track_url, {:queued, queue_length}) do
     %{
-      type: 4,
       flags: 64, # Ephemeral
-      data: %{
-        content: "Added track: #{track_url} to the queue. #{queue_length} song(s) now in the queue"
-      }
+      content: "Added track: #{track_url} to the queue. #{queue_length} song(s) now in the queue"
     }
   end
 
   defp error_response(msg, track_url) do
     %{
-      type: 4,
       flags: 64, # Ephemeral
-      data: %{
-        content: "Error #{msg} when playing track: #{track_url}"
-      }
+      content: "Error #{msg} when playing track: #{track_url}"
+    }
+  end
+
+  defp loading_response() do
+    %{
+      type: 5,
     }
   end
 end
